ecfaadc5c1e7556c143a45c26440e22ca621fe32
shovian <hideyoshishofian@gmail.com>
2023-12-17 15:24:06 +0700

    retry

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import React, { useEffect } from "react";
 import { FirebaseContext, app } from "../../(context)/FirebaseContext";
