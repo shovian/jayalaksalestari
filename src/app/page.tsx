c6517a0013f9a48e67ad147b4e2ac5d231dd33c7
shovian <hideyoshishofian@gmail.com>
2023-12-17 15:33:12 +0700

    retry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { useRouter } from "next/navigation";
+import LoginPage from "./auth/LoginPage";
 
 export default function Page() {
-  const router = useRouter();
+  // const router = useRouter();
 
-  // Redirect to "/login" page
-  router.push("/auth");
+  // // Redirect to "/login" page
+  // router.push("/auth");
 
   // Return null or a placeholder component since this page will be redirected
-  return <div></div>;
+  return <LoginPage></LoginPage>;
 }
