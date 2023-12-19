4d1c51b6964e58d06376b8e80b8a1208995eaed0
shovian <hideyoshishofian@gmail.com>
2023-12-19 14:28:23 +0700

    retest

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,78 +11,78 @@ import { User } from "./(entities)/user";
 import { Absensi } from "./(entities)/absensi";
 
 const Dashboard = () => {
-  const id = User.getCurrentUserId();
-  const role = User.getCurrentUserRole();
-  const [alreadyAbsensi, setAlreadyAbsensi] = useState(false);
-  const [currentUserRole, setCurrentUserRole] = useState<string | null>(null);
-  useEffect(() => {
-    Absensi.subscribeStatus(id as string, setAlreadyAbsensi);
-    role == "managergudang" || role == "staffgudang"
-      ? Absensi.getLatestDate().then((date: Date) => {
-          const nowDate = new Date(Date.now());
-          const isLatestDate = date.getDate() === nowDate.getDate();
-          const isLatestMonth = date.getMonth() === nowDate.getMonth();
-          const isLatestFullYear = date.getFullYear() === nowDate.getFullYear();
-          if (isLatestDate && isLatestMonth && isLatestFullYear) {
-            Absensi.getLatestAbsensiStatusByIdKaryawan(
-              User.getCurrentUserId()
-            ).then((status) => {
-              setAlreadyAbsensi(status === "hadir");
-            });
-          } else {
-            if (nowDate.getHours() >= 8) {
-              Absensi.assignNewAbsensi();
-            }
-          }
-        })
-      : {};
-    const tempUserRole = role;
-    if (tempUserRole !== undefined) {
-      setCurrentUserRole(tempUserRole);
-    }
-  }, []);
-  const pageList =
-    currentUserRole === ""
-      ? [
-          { name: "Daftar Barang", page: <DaftarBarang /> },
-          { name: "Daftar Proyek", page: <DaftarProyek /> },
-          { name: "Absensi", page: <AbsensiPage /> },
-          { name: "Daftar Dana Tersetujui", page: <Pendanaan /> },
-          { name: "Persetujuan Dana", page: <Persetujuan /> },
-          { name: "Pengajuan Dana", page: <Pengajuan /> },
-        ]
-      : currentUserRole === "pemilik"
-      ? [
-          { name: "Daftar Barang", page: <DaftarBarang /> },
-          { name: "Daftar Proyek", page: <DaftarProyek /> },
-          { name: "Absensi", page: <AbsensiPage /> },
-          { name: "Persetujuan Dana", page: <Persetujuan /> },
-        ]
-      : currentUserRole === "adminkeuangan"
-      ? [
-          { name: "Daftar Barang", page: <DaftarBarang /> },
-          { name: "Daftar Proyek", page: <DaftarProyek /> },
-          { name: "Daftar Dana Tersetujui", page: <Pendanaan /> },
-        ]
-      : currentUserRole === "adminhrd"
-      ? [
-          { name: "Daftar Barang", page: <DaftarBarang /> },
-          { name: "Daftar Proyek", page: <DaftarProyek /> },
-          { name: "Absensi", page: <AbsensiPage /> },
-        ]
-      : currentUserRole === "managergudang"
-      ? [
-          { name: "Daftar Barang", page: <DaftarBarang /> },
-          { name: "Daftar Proyek", page: <DaftarProyek /> },
-          { name: "Absensi", page: <AbsensiPage /> },
-          { name: "Pengajuan Dana", page: <Pengajuan /> },
-        ]
-      : [
-          { name: "Daftar Barang", page: <DaftarBarang /> },
-          { name: "Daftar Proyek", page: <DaftarProyek /> },
-          { name: "Absensi", page: <AbsensiPage /> },
-          { name: "Pengajuan Dana", page: <Pengajuan /> },
-        ];
+  // const id = User.getCurrentUserId();
+  // const role = User.getCurrentUserRole();
+  // const [alreadyAbsensi, setAlreadyAbsensi] = useState(false);
+  // const [currentUserRole, setCurrentUserRole] = useState<string | null>(null);
+  // useEffect(() => {
+  //   Absensi.subscribeStatus(id as string, setAlreadyAbsensi);
+  //   role == "managergudang" || role == "staffgudang"
+  //     ? Absensi.getLatestDate().then((date: Date) => {
+  //         const nowDate = new Date(Date.now());
+  //         const isLatestDate = date.getDate() === nowDate.getDate();
+  //         const isLatestMonth = date.getMonth() === nowDate.getMonth();
+  //         const isLatestFullYear = date.getFullYear() === nowDate.getFullYear();
+  //         if (isLatestDate && isLatestMonth && isLatestFullYear) {
+  //           Absensi.getLatestAbsensiStatusByIdKaryawan(
+  //             User.getCurrentUserId()
+  //           ).then((status) => {
+  //             setAlreadyAbsensi(status === "hadir");
+  //           });
+  //         } else {
+  //           if (nowDate.getHours() >= 8) {
+  //             Absensi.assignNewAbsensi();
+  //           }
+  //         }
+  //       })
+  //     : {};
+  //   const tempUserRole = role;
+  //   if (tempUserRole !== undefined) {
+  //     setCurrentUserRole(tempUserRole);
+  //   }
+  // }, []);
+  // const pageList =
+  //   currentUserRole === ""
+  //     ? [
+  //         { name: "Daftar Barang", page: <DaftarBarang /> },
+  //         { name: "Daftar Proyek", page: <DaftarProyek /> },
+  //         { name: "Absensi", page: <AbsensiPage /> },
+  //         { name: "Daftar Dana Tersetujui", page: <Pendanaan /> },
+  //         { name: "Persetujuan Dana", page: <Persetujuan /> },
+  //         { name: "Pengajuan Dana", page: <Pengajuan /> },
+  //       ]
+  //     : currentUserRole === "pemilik"
+  //     ? [
+  //         { name: "Daftar Barang", page: <DaftarBarang /> },
+  //         { name: "Daftar Proyek", page: <DaftarProyek /> },
+  //         { name: "Absensi", page: <AbsensiPage /> },
+  //         { name: "Persetujuan Dana", page: <Persetujuan /> },
+  //       ]
+  //     : currentUserRole === "adminkeuangan"
+  //     ? [
+  //         { name: "Daftar Barang", page: <DaftarBarang /> },
+  //         { name: "Daftar Proyek", page: <DaftarProyek /> },
+  //         { name: "Daftar Dana Tersetujui", page: <Pendanaan /> },
+  //       ]
+  //     : currentUserRole === "adminhrd"
+  //     ? [
+  //         { name: "Daftar Barang", page: <DaftarBarang /> },
+  //         { name: "Daftar Proyek", page: <DaftarProyek /> },
+  //         { name: "Absensi", page: <AbsensiPage /> },
+  //       ]
+  //     : currentUserRole === "managergudang"
+  //     ? [
+  //         { name: "Daftar Barang", page: <DaftarBarang /> },
+  //         { name: "Daftar Proyek", page: <DaftarProyek /> },
+  //         { name: "Absensi", page: <AbsensiPage /> },
+  //         { name: "Pengajuan Dana", page: <Pengajuan /> },
+  //       ]
+  //     : [
+  //         { name: "Daftar Barang", page: <DaftarBarang /> },
+  //         { name: "Daftar Proyek", page: <DaftarProyek /> },
+  //         { name: "Absensi", page: <AbsensiPage /> },
+  //         { name: "Pengajuan Dana", page: <Pengajuan /> },
+  //       ];
   // return currentUserRole ? (
   //   alreadyAbsensi ? (
   //     <div className="text-xs lg:text-base">
